7d55cd91a8d04544ea076cb6dbe8489dd31392dd
akidon0000 <akidon0000@akidon0000noMacBook-Pro.local>
2024-10-08 16:43:46 +0900

    基本機能完成

---
+++ Source/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  iSimCameraTool
+//
+//  Created by akidon0000 on 2024/10/07.
+//
+
+func main() {
+    let cameraManager = CameraManager()
+    cameraManager.startCaptureSession()
+}
+
+main()
